c51401392c255033c8423fde765d3c6fe3f7adf4
RJiazhen <jiazhen.ruan@outlook.com>
2025-03-17 23:08:49 +0800

    feat: add navigation links to normal Nuxt and client render pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <h1>Hello World</h1>
+    <ul>
+      <li>
+        <NuxtLink to="/normal-nuxt-page">Normal Nuxt Page</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/client-render-page">Client Render Page</NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
 
