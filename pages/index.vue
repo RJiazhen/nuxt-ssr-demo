0b9fc60b6f75633e02e2b511e7cddddf7081f803
RJiazhen <jiazhen.ruan@outlook.com>
2025-03-19 22:37:12 +0800

    feat: redesign index page layout with dynamic route links and improved styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
-    <ul>
-      <li>
-        <NuxtLink to="/all-ssr-page">All SSR Page</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/client-render-page">Client Render Page</NuxtLink>
-      </li>
-    </ul>
+  <div class="container mx-auto p-4">
+    <h1 class="text-4xl font-bold mb-8 text-center text-blue-500">
+      Nuxt SSR Test
+    </h1>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <NuxtLink
+        v-for="page in filteredRoutes"
+        :key="page.path"
+        :to="page.path"
+        class="text-2xl font-semibold bg-gradient-to-r from-blue-500 to-purple-500 p-6 rounded-lg shadow-lg flex items-center text-white justify-center hover:cursor-pointer hover:shadow-xl transition-all duration-300 no-underline"
+      >
+        {{ page.name }}
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup>
+const router = useRouter();
+const routes = router.getRoutes();
+const filteredRoutes = routes.filter((route) => route.name !== 'index');
+</script>
